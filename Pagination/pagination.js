6acfc1aba04137a19d9b777fbb599432bb84e3ae
Trevor Cheung <tcheung@bayviewglen.ca>
2021-10-07 10:15:50 -0400

    WIP Pagination - filtering and sorting works for the most part (still need to un-hardcode the page buttons)

---
+++ Pagination/pagination.js
@@ -0,0 +1,103 @@
+const users = '[{"id":2490,"name":"dyson 201","email":"kkr@csk.org","gender":"female","status":"active"},{"id":2496,"name":"Yuvraj","email":"Yuvraj@synthetic.com","gender":"male","status":"inactive"},{"id":2500,"name":"Tenali Ramakrishna","email":"Cyril_Turcotte@gmail.com","gender":"male","status":"active"},{"id":2484,"name":"Kakan","email":"tdesi@gmail.com","gender":"male","status":"inactive"},{"id":2480,"name":"Ahmed","email":"testfamo@gmail.com","gender":"male","status":"active"},{"id":2487,"name":"AhmedTest","email":"ahmed@gmail.com","gender":"male","status":"inactive"},{"id":2491,"name":"Ahmed10put","email":"ahmed10put@synthetic.com","gender":"male","status":"inactive"},{"id":2505,"name":"API Monitoring:xo3c9c","email":"apimonitoringxo3c9c@synthetic.com","gender":"male","status":"active"},{"id":2506,"name":"Anil Kumari","email":"anil13@ccbp.tech","gender":"male","status":"inactive"},{"id":2485,"name":"TestKakanj","email":"pb95@gmail.com","gender":"male","status":"inactive"},{"id":2488,"name":"API Monitoring:3zwgea1111","email":"apimonitoring3zwgea1111@synthetic.com","gender":"male","status":"active"},{"id":2503,"name":"Tenali Ramakrishna","email":"tenali.ramakrishna@21ce.com","gender":"male","status":"active"},{"id":2481,"name":"DESIRE","email":"desi@gmail.com","gender":"male","status":"active"},{"id":2482,"name":"API Monitoring:swa59e","email":"apimonitoringswa59e@synthetic.com","gender":"male","status":"active"},{"id":122,"name":"Aadidev Jain","email":"jain_aadidev@schamberger.biz","gender":"male","status":"inactive"},{"id":103,"name":"Tanya Rana","email":"rana_tanya@douglas.co","gender":"female","status":"active"},{"id":107,"name":"Mr. Lavanya Chopra","email":"lavanya_mr_chopra@mcglynn.co","gender":"male","status":"inactive"},{"id":108,"name":"Lakshman Gupta","email":"gupta_lakshman@aufderhar-kuphal.org","gender":"female","status":"inactive"},{"id":109,"name":"Sarada Bhat DC","email":"bhat_dc_sarada@stark.io","gender":"male","status":"active"},{"id":110,"name":"Narayan Gill","email":"gill_narayan@krajcik.org","gender":"male","status":"active"},{"id":111,"name":"Kanishka Patil","email":"patil_kanishka@wyman-lehner.net","gender":"female","status":"active"},{"id":112,"name":"Ahilya Kocchar","email":"kocchar_ahilya@okon.info","gender":"female","status":"inactive"},{"id":113,"name":"Chitraksh Mehra","email":"mehra_chitraksh@gislason-littel.co","gender":"female","status":"active"},{"id":114,"name":"Dhyanesh Panicker V","email":"v_panicker_dhyanesh@ullrich.co","gender":"female","status":"active"},{"id":115,"name":"Bhaswar Abbott IV","email":"abbott_iv_bhaswar@reilly.com","gender":"male","status":"inactive"},{"id":116,"name":"Sanya Rana","email":"sanya_rana@dietrich-mante.co","gender":"male","status":"inactive"},{"id":117,"name":"Sen. Ganesh Khan","email":"sen_khan_ganesh@reichert.name","gender":"male","status":"active"},{"id":118,"name":"Vasudha Nambeesan","email":"vasudha_nambeesan@oconner.name","gender":"female","status":"inactive"},{"id":119,"name":"Acaryatanaya Singh VM","email":"vm_acaryatanaya_singh@huels-buckridge.co","gender":"male","status":"active"},{"id":2141,"name":"pocrobot test","email":"mail_20211007_51@gmail.com","gender":"male","status":"active"},{"id":121,"name":"Annapurna Tandon","email":"annapurna_tandon@metz-bins.info","gender":"male","status":"inactive"},{"id":124,"name":"Msgr. Vimala Pilla","email":"vimala_msgr_pilla@williamson.com","gender":"male","status":"active"},{"id":125,"name":"Dipesh Bharadwaj","email":"dipesh_bharadwaj@schneider-runolfsdottir.com","gender":"female","status":"active"},{"id":126,"name":"Mina Dubashi PhD","email":"phd_mina_dubashi@cormier.io","gender":"male","status":"inactive"},{"id":129,"name":"Dr. Shivakari Trivedi","email":"shivakari_trivedi_dr@beahan.net","gender":"male","status":"inactive"},{"id":130,"name":"Dr. Bhoopati Johar","email":"bhoopati_dr_johar@schuppe-goyette.biz","gender":"female","status":"inactive"},{"id":131,"name":"Agniprava Tagore","email":"agniprava_tagore@bailey.com","gender":"female","status":"active"},{"id":132,"name":"Anjushri Somayaji","email":"somayaji_anjushri@weber-crona.co","gender":"male","status":"active"},{"id":133,"name":"The Hon. Acaryatanaya Khanna","email":"hon_the_acaryatanaya_khanna@barton.info","gender":"female","status":"inactive"},{"id":134,"name":"Damayanti Varma","email":"damayanti_varma@west-nienow.com","gender":"female","status":"active"},{"id":135,"name":"Priyala Shah","email":"priyala_shah@walsh.io","gender":"female","status":"inactive"},{"id":136,"name":"Bhudev Prajapat","email":"bhudev_prajapat@kreiger.net","gender":"male","status":"inactive"},{"id":137,"name":"Devasree Kakkar","email":"devasree_kakkar@kemmer.info","gender":"male","status":"active"},{"id":138,"name":"Brahmdev Reddy","email":"reddy_brahmdev@mcglynn.info","gender":"male","status":"inactive"},{"id":139,"name":"Rajan Shukla","email":"rajan_shukla@wolf.org","gender":"female","status":"active"},{"id":140,"name":"Rohana Reddy","email":"rohana_reddy@kerluke.io","gender":"female","status":"active"},{"id":141,"name":"Chanda Asan LLD","email":"lld_chanda_asan@zemlak.name","gender":"male","status":"inactive"},{"id":142,"name":"Opaline Jain DC","email":"opaline_dc_jain@stark-koelpin.com","gender":"female","status":"active"},{"id":143,"name":"Shresthi Gill","email":"gill_shresthi@simonis.biz","gender":"female","status":"active"},{"id":144,"name":"Udai Kaur","email":"kaur_udai@batz.co","gender":"female","status":"inactive"},{"id":145,"name":"Bandhu Pothuvaal","email":"pothuvaal_bandhu@damore.net","gender":"male","status":"inactive"},{"id":2461,"name":"Testy Testev","email":"test@Isaias2.io","gender":"male","status":"inactive"},{"id":2468,"name":"API Monitoring:auooh","email":"apimonitoringauooh@synthetic.com","gender":"male","status":"active"},{"id":2470,"name":"fghhj","email":"ggg@uuu.com","gender":"female","status":"inactive"},{"id":2471,"name":"Geos team","email":"fake.email@example.com","gender":"male","status":"active"},{"id":2472,"name":"Testy Testev","email":"test@Isaia03.io","gender":"male","status":"inactive"},{"id":2473,"name":"Miss Ariola Idowu","email":"iyer_ariola_miss@price.biz","gender":"male","status":"active"},{"id":2474,"name":"API Monitoring:bqm5ic","email":"apimonitoringbqm5ic@synthetic.com","gender":"male","status":"active"},{"id":2476,"name":"fwds","email":"wfds@gmail.com","gender":"female","status":"active"},{"id":146,"name":"Tanushri Kaur","email":"kaur_tanushri@abshire-kozey.info","gender":"female","status":"inactive"},{"id":147,"name":"Vrinda Reddy VM","email":"reddy_vrinda_vm@hyatt.com","gender":"female","status":"active"},{"id":148,"name":"Subhash Ahluwalia","email":"ahluwalia_subhash@gutmann.name","gender":"male","status":"inactive"},{"id":149,"name":"Shashikala Reddy","email":"reddy_shashikala@hand.name","gender":"male","status":"inactive"},{"id":150,"name":"Ms. Anamika Agarwal","email":"ms_agarwal_anamika@hermann.co","gender":"female","status":"active"},{"id":151,"name":"Bhadra Chopra PhD","email":"bhadra_chopra_phd@erdman-kirlin.org","gender":"male","status":"active"},{"id":152,"name":"Chapala Asan","email":"chapala_asan@brakus.biz","gender":"male","status":"active"},{"id":153,"name":"Fr. Ranjit Nambeesan","email":"nambeesan_ranjit_fr@schuppe.io","gender":"female","status":"active"},{"id":154,"name":"Bankimchandra Gupta","email":"gupta_bankimchandra@trantow.org","gender":"male","status":"inactive"},{"id":155,"name":"Dhanadeepa Embranthiri","email":"embranthiri_dhanadeepa@fahey.net","gender":"female","status":"inactive"},{"id":2493,"name":"API Monitoring:15b1oq","email":"apimonitoring15b1oq@synthetic.com","gender":"male","status":"active"},{"id":2495,"name":"vilas123","email":"rathod1@gmail.com","gender":"male","status":"active"},{"id":2497,"name":"Tenali Ramakrishna","email":"Karina.Blanda15@yahoo.com","gender":"male","status":"active"},{"id":165,"name":"Amodini Adiga","email":"amodini_adiga@bode.info","gender":"female","status":"active"},{"id":166,"name":"Ekaparnika Rana","email":"rana_ekaparnika@ratke-roob.net","gender":"female","status":"active"},{"id":167,"name":"Himadri Tandon I","email":"himadri_tandon_i@gleason-volkman.com","gender":"female","status":"active"},{"id":168,"name":"Susheel Guneta","email":"guneta_susheel@gulgowski-haag.name","gender":"female","status":"active"},{"id":169,"name":"Miss Prasad Mukhopadhyay","email":"prasad_mukhopadhyay_miss@schneider.org","gender":"female","status":"inactive"},{"id":171,"name":"Bhaaswar Patil","email":"patil_bhaaswar@rippin.co","gender":"female","status":"active"},{"id":172,"name":"Bhoj Gowda","email":"bhoj_gowda@powlowski-halvorson.name","gender":"female","status":"active"},{"id":173,"name":"Aatreya Reddy","email":"reddy_aatreya@collier.org","gender":"male","status":"active"},{"id":174,"name":"Agrata Desai","email":"agrata_desai@schoen.name","gender":"male","status":"inactive"},{"id":175,"name":"Anjali Asan","email":"asan_anjali@ruecker.org","gender":"female","status":"active"},{"id":176,"name":"Ravi Kapoor","email":"ravi_kapoor@grant.co","gender":"male","status":"active"},{"id":177,"name":"Lal Chaturvedi","email":"lal_chaturvedi@ullrich-larkin.biz","gender":"male","status":"inactive"},{"id":178,"name":"Ashlesh Pilla","email":"pilla_ashlesh@schroeder.info","gender":"male","status":"active"},{"id":179,"name":"Ganak Shah","email":"shah_ganak@west-hills.co","gender":"male","status":"inactive"},{"id":180,"name":"Mrs. Bahula Mahajan","email":"mahajan_mrs_bahula@kshlerin-mraz.com","gender":"male","status":"inactive"},{"id":181,"name":"Chakravartee Joshi Esq.","email":"chakravartee_joshi_esq@bechtelar-macejkovic.biz","gender":"male","status":"active"},{"id":2499,"name":"API Monitoring:tba8f","email":"apimonitoringtba8f@synthetic.com","gender":"male","status":"active"},{"id":182,"name":"Himadri Arora","email":"arora_himadri@mayer.net","gender":"male","status":"active"},{"id":183,"name":"Asha Verma","email":"verma_asha@lowe.biz","gender":"female","status":"inactive"},{"id":184,"name":"Esha Verma Ret.","email":"ret_verma_esha@streich-haley.biz","gender":"male","status":"inactive"},{"id":185,"name":"Meena Malik","email":"malik_meena@grimes-spencer.name","gender":"female","status":"active"},{"id":186,"name":"Chakrika Kaniyar","email":"kaniyar_chakrika@weissnat.name","gender":"male","status":"active"},{"id":187,"name":"Harita Kakkar DDS","email":"harita_kakkar_dds@pagac.name","gender":"male","status":"active"},{"id":188,"name":"Baidehi Bandopadhyay Esq.","email":"esq_baidehi_bandopadhyay@hagenes-feeney.co","gender":"female","status":"inactive"},{"id":189,"name":"Avani Bhattathiri","email":"avani_bhattathiri@gleichner.org","gender":"female","status":"inactive"},{"id":190,"name":"Aasha Dutta","email":"aasha_dutta@quitzon-powlowski.org","gender":"female","status":"inactive"},{"id":191,"name":"Dipesh Mahajan IV","email":"dipesh_iv_mahajan@harvey-romaguera.biz","gender":"male","status":"active"},{"id":192,"name":"Aanandinii Chopra","email":"chopra_aanandinii@hudson.co","gender":"male","status":"active"},{"id":193,"name":"Manjusha Sharma","email":"manjusha_sharma@keeling.name","gender":"male","status":"inactive"},{"id":194,"name":"Indra Bhat","email":"indra_bhat@crona.com","gender":"female","status":"inactive"},{"id":195,"name":"Bhisham Adiga","email":"bhisham_adiga@raynor.info","gender":"female","status":"inactive"},{"id":196,"name":"Vedang Trivedi","email":"trivedi_vedang@kirlin-corwin.net","gender":"male","status":"active"},{"id":197,"name":"Dhananjay Arora","email":"arora_dhananjay@rempel.info","gender":"male","status":"inactive"},{"id":198,"name":"Mohan Nair","email":"mohan_nair@okuneva.net","gender":"female","status":"active"},{"id":199,"name":"Ambar Bandopadhyay","email":"ambar_bandopadhyay@cummerata.co","gender":"female","status":"active"},{"id":200,"name":"Dhana Gupta","email":"dhana_gupta@bayer.biz","gender":"male","status":"active"},{"id":201,"name":"Brajendra Ganaka","email":"brajendra_ganaka@ortiz.info","gender":"male","status":"inactive"},{"id":202,"name":"Satish Deshpande","email":"deshpande_satish@oreilly.info","gender":"male","status":"inactive"},{"id":203,"name":"Gov. Arnesh Johar","email":"gov_arnesh_johar@brakus-wintheiser.io","gender":"female","status":"inactive"},{"id":204,"name":"Tejas Iyer PhD","email":"phd_iyer_tejas@crooks.info","gender":"female","status":"inactive"},{"id":205,"name":"Bhushan Kaur","email":"bhushan_kaur@lynch-stehr.co","gender":"male","status":"active"},{"id":206,"name":"Pres. Vasudha Dwivedi","email":"vasudha_pres_dwivedi@gottlieb.io","gender":"female","status":"active"},{"id":207,"name":"Satish Rana","email":"satish_rana@prosacco.org","gender":"male","status":"inactive"},{"id":208,"name":"Jyotsana Saini","email":"saini_jyotsana@predovic.info","gender":"female","status":"active"},{"id":209,"name":"Shashi Varrier DO","email":"shashi_do_varrier@berge.biz","gender":"male","status":"inactive"},{"id":210,"name":"Ujjwal Devar","email":"ujjwal_devar@franecki.io","gender":"male","status":"inactive"},{"id":211,"name":"Balaaditya Asan","email":"balaaditya_asan@gerlach.co","gender":"male","status":"active"},{"id":212,"name":"Rep. Dayamayee Pilla","email":"dayamayee_rep_pilla@bruen-steuber.net","gender":"female","status":"inactive"}]';
+
+let userRecords = JSON.parse(users);
+let slicedRecords = [];
+let filteredRecords = [];
+let slicedFilteredRecords = [];
+filterValue = '';
+
+let currentPage = 1;
+
+let currentSortField = 'name';
+let sortDirection = 'DESC';
+
+sort('name');
+
+console.log(userRecords);
+
+function sliceUserRecords (page){
+    slicedRecords = userRecords.slice(10 * (page - 1), 10 * page);
+}
+
+function sliceFilteredUserRecords(page) {
+    slicedFilteredRecords = filteredRecords.slice(10 * (page - 1), 10 * page);
+}
+
+function getTenUsers(page) {
+    document.querySelector('#rows').innerHTML = '';
+    filterValue = document.querySelector('#filter').value;
+    if (filterValue != '') {
+        sliceFilteredUserRecords(page);
+        currentPage = page;
+        slicedFilteredRecords.forEach(user => { let row = createRow(user); document.querySelector('#rows').appendChild(row); })
+    } else {
+        sliceUserRecords(page);
+        currentPage = page;
+        slicedRecords.forEach(user => { let row = createRow(user); document.querySelector('#rows').appendChild(row); });
+    }
+}
+
+function createRow(user) {
+    let newRow = document.createElement('tr');
+    newRow.setAttribute('scope', 'row');
+    let newCell = document.createElement('td');
+    newCell.appendChild(document.createTextNode(user.name));
+    newRow.appendChild(newCell);
+    newCell = document.createElement('td');
+    newCell.appendChild(document.createTextNode(user.email));
+    newRow.appendChild(newCell);
+    newCell = document.createElement('td');
+    newCell.appendChild(document.createTextNode(user.gender));
+    newRow.appendChild(newCell);
+    newCell = document.createElement('td');
+    newCell.appendChild(document.createTextNode(user.status));
+    newRow.appendChild(newCell);
+ 
+    return newRow;
+}
+
+function sort(field) {
+    if (field === undefined) {
+       // called from filter
+       field = currentSortField;
+    } else if (field === currentSortField) {
+       sortDirection = sortDirection === 'ASC' ? 'DESC' : 'ASC';
+       currentSortField = field;
+    } else {
+       sortDirection = 'ASC';
+       currentSortField = field;
+    }
+ 
+    if (sortDirection === 'ASC') {
+       if (field === 'name') {
+          userRecords.sort((a, b) => a.name.localeCompare(b.name));
+       } else if (field === 'email') {
+          userRecords.sort((a, b) => a.email.localeCompare(b.email));
+       } else if (field === 'gender') {
+          userRecords.sort((a, b) => a.gender.localeCompare(b.gender));
+       } else if (field === 'status') {
+          userRecords.sort((a, b) => a.status.localeCompare(b.status));
+       }
+    } else if (sortDirection === 'DESC') {
+       if (field === 'name') {
+          userRecords.sort((a, b) => b.name.localeCompare(a.name));
+       } else if (field === 'email') {
+          userRecords.sort((a, b) => b.email.localeCompare(a.email));
+       } else if (field === 'gender') {
+          userRecords.sort((a, b) => b.gender.localeCompare(a.gender));
+       } else if (field === 'status') {
+          userRecords.sort((a, b) => b.status.localeCompare(a.status));
+       }
+    }
+ 
+    getTenUsers(currentPage);
+ }
+
+ function filterUsers() {
+    let filterValue = document.querySelector('#filter').value;
+    filteredRecords = userRecords.filter(user => (user.name.toLowerCase().indexOf(filterValue.toLowerCase())) >= 0);
+    sort(); // field in sort will be undefined
+    getTenUsers(1);
+ }
+
+getTenUsers(1);
