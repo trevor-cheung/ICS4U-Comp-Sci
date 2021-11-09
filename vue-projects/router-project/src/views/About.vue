57a1cbfd1a2e2e200cba8d43c2c5eb4f6450b74f
Trevor Cheung <tcheung@bayviewglen.ca>
2021-11-09 10:26:09 -0500

    Router Project

---
+++ vue-projects/router-project/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1 @click="test">This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'About',
+  methods: {
+    test() {
+      this.$router.push({name:'Home'});
+    }
+  }
+}
+</script>
